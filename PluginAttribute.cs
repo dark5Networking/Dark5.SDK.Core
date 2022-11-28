cc656e1b9c45d354d8ee063cf5691b5abf61ca01
Dark5 Networking <117284187+dark5Networking@users.noreply.github.com>
2022-11-28 10:45:00 +0100

    Add files via upload

---
+++ PluginAttribute.cs
@@ -0,0 +1,29 @@
+using System;
+using JetBrains.Annotations;
+
+namespace Dark5.SDK.Core
+{
+	/// <inheritdoc />
+	/// <summary>
+	/// Specifies the SDK version number the assembly was built against.
+	/// </summary>
+	[PublicAPI]
+	[AttributeUsage(AttributeTargets.Assembly)]
+	public class PluginAttribute : Attribute
+	{
+		/// <summary>
+		/// Gets the target SDK version number.
+		/// </summary>
+		public uint Target { get; }
+
+		/// <inheritdoc />
+		/// <summary>
+		/// Initializes a new instance of the <see cref="PluginAttribute" /> class.
+		/// </summary>
+		/// <param name="target">The target SDK version number.</param>
+		public PluginAttribute(uint target)
+		{
+			this.Target = target;
+		}
+	}
+}
